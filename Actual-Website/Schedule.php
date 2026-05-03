328bc720af2741c2e41dee429f9f56bafe3df80c
Camille-Doering <doerincb0414@uwec.edu>
2026-05-03 16:41:23 -0500

    Added Nav Bar and Style Sheet to Schedule Page, added Schedule as a nav bar option on all of my pages

--- Actual-Website/Schedule.php
+++ Actual-Website/Schedule.php
@@ -1,8 +1,31 @@
 <html>
     <head>
-
+        <title>Schedule</title>
+        <link rel = "stylesheet" href = "stylesheet.css">
+        <style>
+        
+        </style>
     </head>
     <body>
+        <header>
+        <h1>Sue Lemke</h1>
+		    <p style="text-align: center;">Recycler by Day, Home cook by night</p>
+        </header>
+        <nav>
+        <div><a href = "home.html">Home Page</a></div>
+        <div><a href = "Accepted-Items.html">Accepted Items</a></div>
+        <div><a href = "Different-Metals.html">Different Metals</a></div>
+        <div><a href = "Freon-Info.html">Freon Info</a></div>
+        <div><a href = "Mission.html">Her Mission</a></div>
+        <div><a href = "How-She-Recycles.html">How She Recycles</a></div>
+        <div><a href = "Carbon-Footprint.html">Reducing Carbon Footprint</a></div>
+        <div><a href = "Recycling-Info.html">Recycling Info</a></div>
+        <div><a href = "Schedule.php">Specialized Recycling Info</a></div>
+        <div><a href = "Tools.html">Tools She Uses</a></div>
+        <div><a href = "Types-Wire.html">Types of Wire</a></div>
+        <div class = "curPage"><a href = "Schedule.php">Scheduling</a></div>
+        </nav>
+        <h1>Scheduling</h1>
         <?php
         
         ?>
