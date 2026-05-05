71c3dc39c48d01173c59803742a4c5ebbc50fde1
Qwertyuiopoptop <stoltjac000@gmail.com>
2026-05-04 22:38:53 -0500

    Updated content for Tools, Wires, & Specialized Info, & started the php database creation script

--- Actual-Website/Schedule.php
+++ Actual-Website/Schedule.php
@@ -2,23 +2,8 @@
     <head>
         <title>Schedule</title>
         <link rel = "stylesheet" href = "stylesheet.css">
-        <style>
-            header h1, header p
-            {
-                color:black;
-            }
-            h1
-            {
-                color:#5FA7B0;;
-            }
-        
-        </style>
     </head>
     <body>
-        <header>
-            <h1>Sue Lemke</h1>
-		    <p style="text-align: center;">Preventing waste, one scrap at a time</p>
-        </header>
 		<nav>
 			<a href="home.html"><h3>Home Page</h3></a>
 			<a href="Accepted-Items.html"><h3>Accepted Items</h3></a>
@@ -32,18 +17,48 @@
 			<a href="Types-Wire.html"><h3>Types of Wire</h3></a>
 			<a href="Schedule.php" class="curPage"><h3>Scheduling</h3></a>
 		</nav>
+        <div class="page-header">
+			<img src="photos/Recycle.png" alt="" height="100px" width="100px">
+			<div>
+				<h1>Sue Lemke</h1>
+				<p style="text-align: center;">Preventing waste, one scrap at a time</p>
+			</div>
+			<img src="photos/Recycle.png" alt="" height="100px" width="100px">
+		</div>
+		<hr>
 
         <h1>Scheduling</h1>
+		<!-- echo "<script>console.log()</script>"; -->
         <?php
         
         $servername = "localhost";
         $username = "root";
-        $password = "";
+        $password = "PneuMonoUltraMicroScopicSilicoVolcanoConIoSis!?";
         mysqli_report(MYSQLI_REPORT_OFF);
         $conn = new mysqli($servername, $username, $password);
         if ($conn->connect_error){
             die("Connection failed: ".$conn->connect_error);
         }
+
+        $sql = "CREATE DATABASE time_table";
+
+		if($conn->query($sql) == TRUE){
+				echo "<script>console.log('Database created successfully')</script>";
+		}
+		else{
+				echo "<script>console.log('Error Creating Database:'.$conn->error)</script>";
+		}
+
+		$sql = "USE time_table";
+
+		if($conn->query($sql) == TRUE){
+			echo "<script>console.log('Database changed successfully')</script>";
+		}
+		else{
+			echo "<script>console.log('Error changing database: ' .$conn->error)</script>";
+		}
+
+		$sql = "CREATE TABLE times (logID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, company_name VARCHAR(50) NOT NULL, address VARCHAR(255) NOT NULL, time int";
 
         $conn->close(); 
         ?>
